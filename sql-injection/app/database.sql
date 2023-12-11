06aea8272746017fc7304d8c31dc073a63492fde
Andrew Roberts <andrew@azroberts.com>
2023-12-11 18:08:50 -0500

    SQL Injection: Added database tables and php user

---
+++ sql-injection/app/database.sql
@@ -0,0 +1,40 @@
+/* Create Database */
+CREATE DATABASE IF NOT EXISTS app;
+USE app;
+
+/* Create Tables */
+CREATE TABLE IF NOT EXISTS Users(
+    UserID smallint(6) unsigned NOT NULL AUTO_INCREMENT,
+    Username varchar(30) NOT NULL,
+    Password varchar(60) NOT NULL,
+    PRIMARY KEY (UserID),
+    UNIQUE (Username)
+);
+
+CREATE TABLE IF NOT EXISTS Topics(
+    TopicID smallint(6) unsigned NOT NULL AUTO_INCREMENT,
+    Title varchar(120) NOT NULL,
+    PRIMARY KEY (TopicID)
+);
+
+CREATE TABLE IF NOT EXISTS Posts(
+    PostID smallint(6) unsigned NOT NULL AUTO_INCREMENT,
+    TopicID smallint(6) unsigned NOT NULL,
+    UserID smallint(6) unsigned NOT NULL,
+    Created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    Title varchar(120) NOT NULL,
+    Contents varchar(16000) NOT NULL,
+    PRIMARY KEY (PostID),
+    FOREIGN KEY (TopicID) REFERENCES Topics(TopicID),
+    FOREIGN KEY (UserID) REFERENCES Users(UserID)
+);
+
+/* Create PHP user and grant permissions */
+CREATE USER IF NOT EXISTS 'php'@'localhost' IDENTIFIED BY 'SuperSecurePassword';
+GRANT SELECT, INSERT ON app.Users TO 'php'@'localhost';
+GRANT SELECT, INSERT ON app.Posts TO 'php'@'localhost';
+GRANT SELECT, INSERT ON app.Topics TO 'php'@'localhost';
+FLUSH PRIVILEGES;
+
+/* Populate the database with contents */
+INSERT INTO Users (Username, Password) VALUES ('admin', 'mypassword');
