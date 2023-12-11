d8aa298f2c68e668ec1af2a480822cf99783045f
Andrew Roberts <andrew@azroberts.com>
2023-12-11 18:51:06 -0500

    SQL Injection: Added php-mysqli & successful php mysql request

--- sql-injection/static/index.php
+++ sql-injection/static/index.php
@@ -1,3 +1,18 @@
+<?php
+$servername = "localhost";
+$username = "php";
+$password = "SuperSecurePassword";
+$dbname = "app";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "SELECT * FROM Users WHERE Username = '" . $_GET["name"] . "';";
+$result = $conn->query($sql);
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -6,6 +21,20 @@
     <title>Hello World Application</title>
 </head>
 <body>
-    <p><?php echo "Hello, " . $_GET["name"] . "!"; ?></p>
+    <?php
+
+    if($result->num_rows > 0) {
+        while($row = $result->fetch_assoc()) {
+            echo "<p>UserID: " . $row["UserID"] . "; Username: " . $row["Username"] . "; Password: " . $row["Password"] . ";</p>";
+        }
+    } else {
+        echo "<p>No results found for " . $_GET["name"] . "</p>";
+    }
+
+    ?>
 </body>
 </html>
+
+<?php
+$conn->close();
+?>
